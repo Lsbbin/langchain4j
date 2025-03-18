b24c43fd9471d326af56ab87686532f1cc15aa19
이성빈 <sungbin0924@gmail.com>
2025-03-18 15:40:20 +0900

    이전 대화 내용 기억 ChatMemory

--- src/main/java/com/langchain/langchain4j/controller/MistralExample.java
+++ src/main/java/com/langchain/langchain4j/controller/MistralExample.java
@@ -1,20 +1,31 @@
 package com.langchain.langchain4j.controller;
 
+import dev.langchain4j.chain.ConversationalChain;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.ollama.OllamaChatModel;
-import org.springframework.stereotype.Controller;
 
 import java.time.Duration;
 import java.util.Scanner;
 
-@Controller
 public class MistralExample {
+
+    private static String MODEL_NAME = "llama3.2"; // Ollama에서 실행할 모델 이름
+    private static String OLLAMA_URL = "http://localhost:11434"; // Ollama 기본 포트
 
     public static void main(String[] args) throws Exception {
         // Ollama를 사용하여 llama3.2 실행
         OllamaChatModel model = OllamaChatModel.builder()
-                .modelName("llama3.2")  // Ollama에서 실행할 모델 이름
-                .baseUrl("http://localhost:11434") // Ollama 기본 포트
+                .modelName(MODEL_NAME)
+                .baseUrl(OLLAMA_URL)
                 .timeout(Duration.ofSeconds(120))
+                .build();
+
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(50);
+
+        ConversationalChain chain = ConversationalChain.builder()
+                .chatLanguageModel(model)
+                .chatMemory(chatMemory)
                 .build();
 
         // 사용자 입력을 받기 위한 Scanner 객체 생성
@@ -32,7 +43,8 @@ public class MistralExample {
             }
 
             // 모델에 질문을 보내고 응답 받기
-            String answer = model.generate(question);
+            //String answer = model.generate(question);
+            String answer = chain.execute(question);
             System.out.println("Llama 3: " + answer);
         }
 
