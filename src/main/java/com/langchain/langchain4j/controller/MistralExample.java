d9b21bc4cc12d7bf46083757affc0c7d9b0fa40a
이성빈 <sungbin0924@gmail.com>
2025-03-18 17:27:45 +0900

    AI 어시스턴트 사용, 스트리밍 응답

--- src/main/java/com/langchain/langchain4j/controller/MistralExample.java
+++ src/main/java/com/langchain/langchain4j/controller/MistralExample.java
@@ -1,11 +1,12 @@
 package com.langchain.langchain4j.controller;
 
-import dev.langchain4j.chain.ConversationalChain;
-import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
 
-import java.time.Duration;
 import java.util.Scanner;
 
 public class MistralExample {
@@ -13,27 +14,27 @@ public class MistralExample {
     private static String MODEL_NAME = "llama3.2"; // Ollama에서 실행할 모델 이름
     private static String OLLAMA_URL = "http://localhost:11434"; // Ollama 기본 포트
 
-    public static void main(String[] args) throws Exception {
+    public static void main (String[] args) throws Exception {
+        interface Assistant  {
+            TokenStream chat(String message);
+        }
+
         // Ollama를 사용하여 llama3.2 실행
-        OllamaChatModel model = OllamaChatModel.builder()
-                .modelName(MODEL_NAME)
+        StreamingChatLanguageModel model = OllamaStreamingChatModel.builder()
                 .baseUrl(OLLAMA_URL)
-                .timeout(Duration.ofSeconds(120))
+                .modelName(MODEL_NAME)
                 .build();
 
-        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(50);
-
-        ConversationalChain chain = ConversationalChain.builder()
-                .chatLanguageModel(model)
-                .chatMemory(chatMemory)
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .streamingChatLanguageModel(model)
+                .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
                 .build();
 
         // 사용자 입력을 받기 위한 Scanner 객체 생성
         Scanner scanner = new Scanner(System.in);
-        System.out.println("질문을 입력하세요:");
+        System.out.print("질문을 입력하세요 : ");
 
         while (true) {
-            System.out.print("> ");
             String question = scanner.nextLine();
 
             // "exit" 또는 "quit" 입력 시 프로그램 종료
@@ -43,9 +44,19 @@ public class MistralExample {
             }
 
             // 모델에 질문을 보내고 응답 받기
-            //String answer = model.generate(question);
-            String answer = chain.execute(question);
-            System.out.println("Llama 3: " + answer);
+            TokenStream tokenStream = assistant.chat(question);
+            tokenStream
+                    //.onRetrieved((List<Content> contents) -> System.out.println(contents))
+                    //.onToolExecuted((ToolExecution toolExecution) -> System.out.println(toolExecution))
+                    .onPartialResponse((String partialResponse) -> {
+                        System.out.print(partialResponse);
+                    })
+                    .onCompleteResponse((ChatResponse response) -> {
+                        System.out.println();
+                        System.out.print("> ");
+                    })
+                    .onError((Throwable error) -> error.printStackTrace())
+                    .start();
         }
 
         scanner.close();
