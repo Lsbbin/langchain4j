9221a1bb0bc7ea123bd3fc5579df2da8b6501b46
이성빈 <sungbin0924@gmail.com>
2025-03-24 17:22:06 +0900

    참고 파일 내용 화면 노출

--- src/main/java/com/langchain/langchain4j/controller/MainController.java
+++ src/main/java/com/langchain/langchain4j/controller/MainController.java
@@ -1,13 +1,23 @@
 package com.langchain.langchain4j.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class MainController {
 
+    private static final String FILE_PATH = "C:/Users/peaku/Desktop/sample/embedding/test.txt";
+
     @RequestMapping(value = "/")
-    public String main() {
+    public String main(Model model) throws IOException {
+        Path path = Paths.get(FILE_PATH);
+        model.addAttribute("fileStr", new String(Files.readAllBytes(path)));
         return "index";
     }
 }
