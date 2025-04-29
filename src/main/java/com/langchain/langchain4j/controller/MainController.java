bccab3c02e2d48b59a3d139900bcd1740ed4300a
이성빈 <sungbin0924@gmail.com>
2025-04-29 16:19:19 +0900

    불필요한 소스 및 파일 정리

--- src/main/java/com/langchain/langchain4j/controller/MainController.java
+++ src/main/java/com/langchain/langchain4j/controller/MainController.java
@@ -1,43 +1,14 @@
 package com.langchain.langchain4j.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Controller
 public class MainController {
 
-    private static final String FILE_PATH = "C:/Users/peaku/Desktop/sample/embedding/test.txt";
 
     @RequestMapping(value = "/")
-    public String main(Model model) throws IOException {
-        Path path = Paths.get(FILE_PATH);
-        model.addAttribute("fileStr", new String(Files.readAllBytes(path)));
+    public String main() {
         return "index";
-    }
-
-    @PostMapping(value = "/saveFile")
-    public ResponseEntity saveFile(@RequestParam("content") String content) {
-        try {
-            File file = new File(FILE_PATH);
-            FileWriter writer = new FileWriter(file);
-            writer.write(content);
-            writer.close();
-            return new ResponseEntity(HttpStatus.OK);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 }
